26665d1f4a8066caeb48f28939f7ac256894443c
Justice Dreischor <i407172@git.fhict.nl>
2021-11-15 16:53:20 +0100

    added feature for adding items to a reservation

--- src/components/molecules/WeaponDialog.tsx
+++ src/components/molecules/WeaponDialog.tsx
@@ -14,19 +14,123 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Checkbox from "@mui/material/Checkbox";
 import Avatar from "@mui/material/Avatar";
+import { alpha, Box, InputBase, styled, Typography } from "@mui/material";
+import { Weapon } from "./WeaponsTableBody";
+import SearchIcon from "@mui/icons-material/Search";
+
+// For testing, mock list of weapons
+const weapons: Weapon[] = [
+  {
+    category_id: 2,
+    created_at: "",
+    desired_amount: 10,
+    current_amount: 5,
+    id: 10,
+    image: "string",
+    name: "Weapon1",
+    price: 100,
+    updated_at: "string",
+  },
+  {
+    category_id: 2,
+    created_at: "",
+    desired_amount: 10,
+    current_amount: 5,
+    id: 12,
+    image: "string",
+    name: "Weapon2",
+    price: 100,
+    updated_at: "string",
+  },
+  {
+    category_id: 2,
+    created_at: "",
+    desired_amount: 10,
+    current_amount: 5,
+    id: 23,
+    image: "string",
+    name: "Uzi",
+    price: 100,
+    updated_at: "string",
+  },
+  {
+    category_id: 2,
+    created_at: "",
+    desired_amount: 10,
+    current_amount: 5,
+    id: 34,
+    image: "string",
+    name: "HandGun",
+    price: 100,
+    updated_at: "string",
+  },
+];
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+  },
+}));
 
 interface WeaponFormDialogProps {
   handleClickClose: () => void;
-  handleAddWeapon: (name: string) => void;
+  handleAddWeapons: (weapons: Weapon[]) => void;
   open: boolean;
 }
 
 export default function WeaponFormDialog(props: WeaponFormDialogProps) {
-  const [checked, setChecked] = React.useState([1]);
+  const selectedWeapons1: Weapon[] = [];
+  const [selectedWeapons, setSelectedWeapons] = React.useState(selectedWeapons1);
+  const [searchKeyword, setSearchKeyword] = React.useState("");
+  const [foundWeapons, setFoundWeapons] = React.useState(weapons);
 
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+  const filter = (e: any) => {
+    const keyword = e.target.value;
+
+    if (keyword !== "") {
+      const results = weapons.filter((weapon) => {
+        return weapon.name.toLowerCase().startsWith(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFoundWeapons(results);
+    } else {
+      setFoundWeapons(weapons);
+      // If the text field is empty, show all users
+    }
+
+    setSearchKeyword(keyword);
+  };
+
+  const handleToggle = (value: Weapon) => () => {
+    const currentIndex = selectedWeapons.indexOf(value);
+    const newChecked = [...selectedWeapons];
 
     if (currentIndex === -1) {
       newChecked.push(value);
@@ -34,7 +138,7 @@ export default function WeaponFormDialog(props: WeaponFormDialogProps) {
       newChecked.splice(currentIndex, 1);
     }
 
-    setChecked(newChecked);
+    setSelectedWeapons(newChecked);
   };
 
   return (
@@ -42,37 +146,79 @@ export default function WeaponFormDialog(props: WeaponFormDialogProps) {
       <Dialog open={props.open} onClose={props.handleClickClose}>
         <DialogTitle>Weapons</DialogTitle>
         <DialogContent>
-          <DialogContentText>Search for your weapon by id or name</DialogContentText>
-          <List dense sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-            {[0, 1, 2, 3].map((value) => {
-              const labelId = `checkbox-list-secondary-label-${value}`;
-              return (
-                <ListItem
-                  key={value}
-                  secondaryAction={
-                    <Checkbox
-                      edge="end"
-                      onChange={handleToggle(value)}
-                      checked={checked.indexOf(value) !== -1}
-                      inputProps={{ "aria-labelledby": labelId }}
-                    />
-                  }
-                  disablePadding
-                >
-                  <ListItemButton>
-                    <ListItemAvatar>
-                      <Avatar alt={`Avatar n°${value + 1}`} src={`/static/images/avatar/${value + 1}.jpg`} />
-                    </ListItemAvatar>
-                    <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
+          <DialogContentText>Search by id or name</DialogContentText>
+          <Search onChange={filter} defaultValue={searchKeyword} placeholder="test">
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+          </Search>
+          {foundWeapons && foundWeapons.length > 0 ? (
+            <List dense sx={{ width: "100%" }}>
+              {foundWeapons.map((weapon) => {
+                const labelId = `checkbox-list-secondary-label-${weapon}`;
+                return (
+                  <ListItem
+                    key={weapon.id}
+                    secondaryAction={
+                      <Checkbox
+                        edge="end"
+                        onChange={handleToggle(weapon)}
+                        checked={selectedWeapons.indexOf(weapon) !== -1}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    }
+                    disablePadding
+                  >
+                    <ListItemButton>
+                      <ListItemAvatar>
+                        <Avatar alt={`Avatar n°${weapon.id + 1}`} src={`/static/images/avatar/${weapon.id + 1}.jpg`} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={weapon.name}
+                        secondary={
+                          <React.Fragment>
+                            <Box>
+                              <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                Id: {weapon.id}
+                              </Typography>
+                            </Box>
+                            <Box>
+                              <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                Category: {weapon.category_id}
+                              </Typography>
+                            </Box>
+                          </React.Fragment>
+                        }
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          ) : (
+            <h1>No results found!</h1>
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={props.handleClickClose}>Cancel</Button>
-          <IconButton icon={<CheckIcon />} onHandleClick={props.handleAddWeapon} text={"Add"} />
+          <IconButton
+            icon={<CheckIcon />}
+            onHandleClick={() => {
+              props.handleAddWeapons(selectedWeapons);
+            }}
+            text={"Add"}
+          />
         </DialogActions>
       </Dialog>
     </div>
--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -8,58 +8,64 @@ import AddIcon from "@mui/icons-material/Add";
 import { ItemTableHeader } from "components/molecules/ItemTableHeader";
 import * as React from "react";
 import WeaponFormDialog from "components/molecules/WeaponDialog";
+import { Weapon } from "components/molecules/WeaponsTableBody";
 
 interface ReservationProps {}
 
+const columns: ColumnConfig[] = [
+  {
+    id: "itemId",
+    title: "Id",
+    muiProps: {
+      width: "60px",
+    },
+  },
+  {
+    id: "name",
+    title: "Weapon",
+  },
+  {
+    id: "category",
+    title: "Category",
+  },
+  {
+    id: "qr_barcode",
+    title: "QR/Barcode",
+  },
+];
+
+const rows: GenericTableRow[] = [
+  {
+    name: "AK-47",
+    category: <Chip label="Heavy" />,
+    status: <Chip label="Returned" />,
+  },
+  {
+    name: "M4a1-s",
+    category: <Chip label="Light" />,
+    status: <Chip color="error" label="In use" />,
+  },
+];
+
 const Reservation: React.FC<ReservationProps> = () => {
-  const columns: ColumnConfig[] = [
-    {
-      title: "ItemId",
-      id: "itemId",
-      muiProps: {
-        width: "60px",
-      },
-    },
-    {
-      title: "Item name",
-      id: "name",
-      muiProps: {
-        align: "left",
-        width: "60%",
-      },
-    },
-    {
-      title: "Category",
-      id: "category",
-      muiProps: {
-        align: "center",
-      },
-    },
-    {
-      title: "QR/Barcode",
-      id: "qr_barcode",
-      muiProps: {
-        align: "center",
-        width: "5%",
-      },
-    },
-  ];
+  const genericTableRows: GenericTableRow[] = [];
+  const [rows, setRows] = React.useState(genericTableRows);
 
-  const rows: GenericTableRow[] = [
-    {
-      name: "AK-47",
-      category: <Chip label="Heavy" />,
-      status: <Chip label="Returned" />,
-    },
-    {
-      name: "M4a1-s",
-      category: <Chip label="Light" />,
-      status: <Chip color="error" label="In use" />,
-    },
-  ];
-
-  const addWeapon = (name: string) => {
+  const addWeapons = (weapons: Weapon[]) => {
     handleClickClose();
+    const newRows: GenericTableRow[] = [];
+    if (weapons != null) {
+      for (let i = 0; i < weapons.length; i++) {
+        console.log(weapons[i]);
+        newRows.push({
+          itemId: weapons[i].id.toString(),
+          name: weapons[i].name,
+          category: weapons[i].category_id.toString(),
+          qr_barcode: weapons[i].id.toString(),
+        });
+      }
+    }
+    setRows(newRows);
   };
 
   const [open, setOpen] = React.useState(false);
@@ -88,7 +94,7 @@ const Reservation: React.FC<ReservationProps> = () => {
       <Box sx={reservationStyles.itemsTableBox}>
         <ItemTableHeader onHandleClick={handleClickOpen} />
         <GenericTable columns={columns} rows={rows} />
-        <WeaponFormDialog handleClickClose={handleClickClose} open={open} handleAddWeapon={addWeapon} />
+        <WeaponFormDialog handleClickClose={handleClickClose} open={open} handleAddWeapons={addWeapons} />
       </Box>
     </BodyLayout>
   );
