40c29a009a397355da078c4c2eee9ba17a4474de
Justice Dreischor <t-justice98@hotmail.com>
2021-12-05 00:11:03 +0100

    Update User.ts
    updated user interface

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,8 +1,12 @@
+import { Depot } from "./Depot";
+
 export interface User {
   id: number;
   name: string;
   email: string;
   access_level: string;
+  language: string;
+  depots: Depot[];
   role?: string;
   email_verified_at: Date | null;
   created_at: Date;
