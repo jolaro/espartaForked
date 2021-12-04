a61b8f189af7192b7433690d23acb194d410f07a
Justice Dreischor <t-justice98@hotmail.com>
2021-12-05 00:08:46 +0100

    Update endpoints.config.ts
    updated RequestGroupResponse and ItemTypeResponse

--- src/utils/api_service/endpoints.config.ts
+++ src/utils/api_service/endpoints.config.ts
@@ -1,3 +1,4 @@
+import { Request } from "./../../components/molecules/RequestsTableBody";
 import { User } from "interfaces/User";
 import { number } from "prop-types";
 export interface RandomCatFactResponse {
@@ -15,12 +16,27 @@ interface RequestGroupEndpointData {
   manager_id?: number;
 }
 
+export interface RequestItemResponse {
+  id: number;
+  updated_at: string;
+  created_at: string;
+  item_id: string;
+  item_type_id: string;
+  request_group_id: string;
+  date_borrowed: string;
+  date_due: string;
+  date_returned: string;
+}
+
 export interface RequestGroupResponse {
-  borrower_id: string;
+  borrower_id: number;
   manager_id: string;
   updated_at: string;
   created_at: string;
+  approved: boolean | null;
+  depot_id: string;
   id: string;
+  request_items: RequestItemResponse[];
 }
 
 interface LoginEndpointResponse {
@@ -33,33 +49,20 @@ interface LoginEndpointResponse {
 }
 
 export interface RequestItemData {
-  item_id: number;
+  item_id: string;
   request_group_id: number;
   approved: number;
-  item_type_id?: number;
+  item_type_id?: string;
   date_due?: string;
   date_borrowed?: string;
   date_returned?: string;
 }
 
-export interface RequestItemResponse {
-  item_id: number;
-  item_type_id: number;
-  request_group_id: number;
-  approved: number;
-  date_due: string;
-  date_borrowed: string;
-  date_returned: string;
-  updated_at: string;
-  created_at: string;
-  id: number;
-}
-
-export interface ItemTypesResponse {
-  id: number;
+export interface ItemTypeResponse {
+  id: string;
   name: string;
   price: string;
-  category_id: string;
+  weight_category: string;
   category: string;
   desired_amount: string;
   created_at: string;
@@ -69,7 +72,7 @@ export interface ItemTypesResponse {
 export interface ItemResponse {
   id: number;
   item_type_id: string;
-  item_type: ItemTypesResponse;
+  item_type: ItemTypeResponse;
   serial: string;
   created_at: string;
   updated_at: string;
@@ -111,7 +114,7 @@ export type GetEndpoint =
       method: "GET";
       url: "/api/itemtypes";
       queryParams: undefined;
-      response: ItemTypesResponse[];
+      response: ItemTypeResponse[];
     }
   | {
       method: "GET";
@@ -129,8 +132,8 @@ export type GetEndpoint =
       method: "GET";
       url: "/api/requestgroup";
       queryParams: RequestGroupQueryParams;
-      response: RequestGroupResponse | RequestGroupResponse[];
-    };
+      response: RequestGroupResponse[];
+    }; 
 
 export type PostEndpoint =
   | {
