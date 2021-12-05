ccc5f4244e1416a6f58176c8897ac007fd00bb0b
Justice Dreischor <t-justice98@hotmail.com>
2021-12-05 16:29:57 +0100

    Update Role.ts
    added enum to get roles based on access level code

--- src/interfaces/Role.ts
+++ src/interfaces/Role.ts
@@ -1,5 +1,14 @@
 export enum Role {
+  ADMIN = "Admin",
   COMMANDER = "Commander",
   OFFICER = "Officer",
   TROOP = "Troop",
+}
+
+
+export enum RoleByAccessLevel {
+  ADMIN = "3",
+  COMMANDER = "2",
+  OFFICER = "1",
+  TROOP = "0",
 }
